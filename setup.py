0425ec1a9d0547bc98cc7927f15140fc3ef14fed
Yahya Badran <techtweaking@gmail.com>
2024-04-09 14:29:17 +0200

    fix stdout in csv2yamld

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='yamld',
-    version='0.0.3',
+    version='0.0.33',
     author='Yahya Badran',
     author_email='techtweaking@gmail.com',
     description='parser of YAMLd, a tiny subset of YAML',
-    url='https://github.com/badranx/YAML-2D',
+    url='https://github.com/badranx/yaml-datasets',
     install_requires=['pandas >= 1.0.0'],
     packages = [package for package in find_packages() if package.startswith("yamld")],
     scripts=['bin/csv2yamld', 'bin/yamld2csv', 'bin/yamld'],
